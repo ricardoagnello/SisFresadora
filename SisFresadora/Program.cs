cb086fb3e8c0396c74a765ab291e030275d52459
Ricardo Agnello Neto <ricardo.agnello@outlook.com>
2020-04-09 10:35:28 -0300

    Criação Banco de Dados

--- SisFresadora/Program.cs
+++ SisFresadora/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SisFresadora.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SisFresadora
 {
@@ -13,7 +15,30 @@ namespace SisFresadora
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SisFresadoraContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
