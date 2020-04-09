406ac67fb90f4596772758615e65bfbb0dde015c
Ricardo Agnello Neto <ricardo.agnello@outlook.com>
2020-04-09 09:48:42 -0300

    Criação Classes (cliente e serviço)

---
+++ SisFresadora/Models/Servico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SisFresadora.Models
+{
+    public class Servico
+    {
+        public int ID { get; set; }
+        public int Quantidade { get; set; }
+        public string Tipo { get; set; }
+        public string Material { get; set; }
+        public float Modulo { get; set; }
+        public int NumeroDentes { get; set; }
+        public float ValorUnitario { get; set; }
+        public float ValorTotal
+        {
+            get
+            {
+                return Quantidade * ValorUnitario;
+            }
+        }
+        public string Observacoes { get; set; }
+        public Cliente Cliente { get; set; }
+    }
+}
